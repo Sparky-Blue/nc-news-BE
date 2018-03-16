4db409309cb1e51f76b4cf8dda631f7f879452a6
Clare Pickering <clarelpickering@hotmail.com>
2018-03-16 15:41:41 +0000

    heroku hosting set up

--- listen.js
+++ listen.js
@@ -1,6 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 const app = require("./app");
-const PORT = require("./config/index").PORT[process.env.NODE_ENV];
+const PORT =
+  process.env.NODE_ENV === "production"
+    ? process.env.PORT
+    : require("./config/index").PORT[process.env.NODE_ENV];
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
