99a22144f8ecbea83b573369019107fe4fa70308
Clare Pickering <clarelpickering@hotmail.com>
2018-04-05 14:55:42 +0100

    add cors to server

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ mongoose
   .then(() => console.log(`successfully connected to ${DB_URL}`))
   .catch(err => console.log("connection failed", err));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use("/api", apiRouter);
